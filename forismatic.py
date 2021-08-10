f661548f4faa9cada08240cdbc81cfbc91600087
ANKUSH GOYAL <ankushgoyal310@gmail.com>
2021-08-10 19:40:39 +0530

    Update forismatic.py
    Added print statement for response for debugging

--- forismatic.py
+++ forismatic.py
@@ -9,7 +9,8 @@ url = 'https://api.forismatic.com/api/1.0/?method=getQuote&format=json&lang=en'
 resp = requests.get(url=url)
 
 if resp.status_code == 200:
-    data = json.loads(resp.content)
+    print("API RESPONSE", resp.content)
+    data = resp.json()
 else:
     raise Exception("Failed to fetch data from API")
 
