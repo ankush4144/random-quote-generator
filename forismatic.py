ff0dbb583d08e69f634525205a39d89bffabd4d1
ANKUSH GOYAL <ankushgoyal310@gmail.com>
2021-08-10 19:37:06 +0530

    Update forismatic.py
    Handled code for status code

--- forismatic.py
+++ forismatic.py
@@ -7,8 +7,10 @@ import json
 url = 'https://api.forismatic.com/api/1.0/?method=getQuote&format=json&lang=en'
  
 resp = requests.get(url=url)
-data = json.loads(resp.text)
 
-
+if resp.status_code == 200:
+    data = json.loads(resp.content)
+else:
+    raise Exception("Failed to fetch data from API")
 
 print(data['quoteText'] + '.\n' + data['quoteAuthor'])
