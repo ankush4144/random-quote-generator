d5b2d8a0afc978fbcc2c33e3de74288a0679d899
ANKUSH GOYAL <ankushgoyal310@gmail.com>
2021-08-11 14:48:35 +0530

    Updated response retrieval code from API

--- forismatic.py
+++ forismatic.py
@@ -18,7 +18,7 @@ logging.getLogger().addHandler(console)
 logger = logging.getLogger()
 
  
-url = 'https://api.forismatic.com/api/1.0/?method=getQuote&format=json&lang=en'
+url = 'https://api.forismatic.com/api/1.0/?method=getQuote&format=text&lang=en'
 
 
 logger.info(f"Sending GET request to API - {url}")
@@ -30,17 +30,11 @@ logger.debug(f"API RESPONSE - {resp.content}")
 if resp.status_code == 200:
     logger.info("API response request successful with Status Code - 200")
     logger.info("Attempting to render data received from API response to JSON")
-    try:
-        try:
-            data = resp.json()
-        except json.decoder.JSONDecodeError:
-            data = eval(resp.content)
-    except:
-        raise Exception("Failed to decode the response received from API")
+    # quote response received from API
+    quote = resp.content
 else:
     raise Exception("Failed to fetch data from API")
 
-quote = data['quoteText'] + '.\n' + data['quoteAuthor']
 logger.info(f"Quote received from API \n{quote}")
 
 filename = "quote.txt"
@@ -52,5 +46,5 @@ except Exception as err:
     raise Exception(f"Failed to write the quote to file due to err - {str(err)}")
 
 
-logger.info(f"Successfully wrote quote {quote} to file {filename}")
+logger.info(f"Successfully wrote quote '{quote}' to file '{filename}'")
 
